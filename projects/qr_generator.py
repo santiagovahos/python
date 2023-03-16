2e4ce6fdeb1c31199ea68984bbf9da61034a6715
Santiago Vahos <santiago.munozv@udea.edu.co>
2023-03-15 22:05:53 -0500

    add qr_generator project

---
+++ projects/qr_generator.py
@@ -0,0 +1,15 @@
+import qrcode
+def qr_generator():
+    url=str(input('Enter the url: '))
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qr.png")
+
+qr_generator()
